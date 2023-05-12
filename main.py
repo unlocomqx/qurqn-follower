d0ec2b645a450690fefc800b6504f34b8ee61f41
unloco <amxmodx.unloco@gmail.com>
2023-05-12 23:23:33 +0100

    Add bg listener

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import speech_recognition as sr
+import time
 
 r = sr.Recognizer()
+
 
 def callback(recognizer, audio):
     try:
@@ -10,15 +12,23 @@ def callback(recognizer, audio):
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
 
-with sr.Microphone() as source:
-    print("Say something...")
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
-try:
-    r.pause_threshold = 0.5
-    t = r.recognize_google(audio, language='ar-AR', show_all=True)
-    print(t)
-except sr.UnknownValueError:
-    print("Could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))
+
+r = sr.Recognizer()
+m = sr.Microphone()
+with m as source:
+    r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
+
+# start listening in the background (note that we don't have to do this inside a `with` statement)
+stop_listening = r.listen_in_background(m, callback)
+# `stop_listening` is now a function that, when called, stops background listening
+
+# do some unrelated computations for 5 seconds
+for _ in range(10): time.sleep(0.1)  # we're still listening even though the main thread is doing other things
+
+print("stop listening")
+# calling this function requests that the background listener stop listening
+# stop_listening(wait_for_stop=False)
+
+# do some more unrelated things
+# while True: time.sleep(0.1)
+# we're not listening anymore, even though the background thread might still be running for a second or two while cleaning up and stopping
