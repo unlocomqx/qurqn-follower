eb3f149888a558ca75dab588aadd15f9ba3e7cb2
unloco <amxmodx.unloco@gmail.com>
2023-05-14 13:50:55 +0100

    Add text search

---
+++ search.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr
+import os
+import pandas as pd
+from thefuzz import fuzz
+from thefuzz import process
+
+csv = pd.read_csv('./data/simplified.csv')
+texts = csv['simplified'].to_list()
+
+# print(texts)
+
+r = sr.Recognizer()
+
+while True:
+    with sr.Microphone() as source:
+        print("Say something...")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, phrase_time_limit=3)
+        print("Recognizing...")
+    try:
+        r.pause_threshold = 0.5
+        text = r.recognize_whisper(audio, language='english', model='medium')
+        print(text)
+        # if len(text) > 0:
+        #     result = process.extract(text, texts, limit=3)
+        #     print(result)
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
